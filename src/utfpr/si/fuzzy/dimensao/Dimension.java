c9fed0f1ccd09ac5e31176194076d4afe937b989
henriquermonteiro <henrique.r.monteiro@hotmail.com>
2015-06-08 10:54:22 -0300

    Start

---
+++ src/utfpr/si/fuzzy/dimensao/Dimension.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utfpr.si.fuzzy.dimensao;
+
+/**
+ *
+ * @author henrique
+ */
+public interface Dimension {
+    public void getClassFor();
+}
---
+++ src/utfpr/si/fuzzy/variavel/Variable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utfpr.si.fuzzy.variavel;
+
+/**
+ *
+ * @author henrique
+ */
+public interface Variable {
+    public Comparable getValue();
+}
